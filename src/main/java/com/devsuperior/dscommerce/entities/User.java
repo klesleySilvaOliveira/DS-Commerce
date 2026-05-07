3882468ecc851a3a2e848ebec32a6ba2762686d8
Klesley Silva de Oliveira <klesley.oliveira@hotmail.com>
2026-05-07 13:18:30 -0300

    Project start

---
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -0,0 +1,68 @@
+package com.devsuperior.dscommerce.entities;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String email;
+	private String phone;
+	private LocalDate birthDate;
+	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
+	public User(Long id, String name, String email, String phone, LocalDate birthDate, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.birthDate = birthDate;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
+}
